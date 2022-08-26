33c892b8df8d61bc972588f73baae69e218c31df
Riswan <riswanlufti@gmail.com>
2022-08-26 19:06:47 +0700

    ProjekProfilGuru

---
+++ app/src/main/java/com/smkn1sayung/projekprofilguru/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.smkn1sayung.projekprofilguru
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
